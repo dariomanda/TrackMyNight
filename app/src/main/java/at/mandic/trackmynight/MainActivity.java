850edf8345c9fcb981ec6589d073ff61b98686cd
LPhilipp <p.liebhart@gmx.net>
2017-04-02 21:49:56 +0200

    ImageButton für UserProfil

--- app/src/main/java/at/mandic/trackmynight/MainActivity.java
+++ app/src/main/java/at/mandic/trackmynight/MainActivity.java
@@ -1,7 +1,10 @@
 package at.mandic.trackmynight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imgbtn = (ImageButton) findViewById(R.id.imageButton2);
+
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UserProfilActivity.class));
+            }
+        });
+
+
         int a = 1;
         char test = 'a';
     }
---
+++ app/src/main/java/at/mandic/trackmynight/UserProfilActivity.java
@@ -0,0 +1,13 @@
+package at.mandic.trackmynight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profil);
+    }
+}
