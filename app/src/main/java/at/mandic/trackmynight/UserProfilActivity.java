8ff28b95910cb42579fb70df066e8593e6c5f92f
LPhilipp <p.liebhart@gmx.net>
2017-04-03 21:06:18 +0200

    Eingabefelder für UserProfil + Checkbox nur einfach auswählbar

--- app/src/main/java/at/mandic/trackmynight/UserProfilActivity.java
+++ app/src/main/java/at/mandic/trackmynight/UserProfilActivity.java
@@ -2,6 +2,8 @@ package at.mandic.trackmynight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class UserProfilActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class UserProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profil);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //Checkbox fuer Geschlechtsauswahl, es darf immer nur ein Geschlecht ausgewaehlt werden!!!
+        final CheckBox maennlich = (CheckBox) findViewById(R.id.checkBox);
+        final CheckBox weiblich =  (CheckBox) findViewById(R.id.checkBox2);
+
+        maennlich.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    weiblich.setChecked(false);
+                }
+            }
+        });
+        weiblich.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    maennlich.setChecked(false);
+                }
+            }
+        });
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+
     }
 }
