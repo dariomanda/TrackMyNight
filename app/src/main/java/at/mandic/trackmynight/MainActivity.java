67b66e7f6bf0ceac9dc798609c683be9c52050dd
dariomanda <dariomandic01@gmail.com>
2017-05-16 16:16:34 +0200

    Drinks Activity, pictures added Manifest permissions Tracking - Toggle Button

--- app/src/main/java/at/mandic/trackmynight/MainActivity.java
+++ app/src/main/java/at/mandic/trackmynight/MainActivity.java
@@ -8,12 +8,16 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
